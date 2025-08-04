6f2ec24a532fa3f957ebc145d27fca427b5ffa18
Michelo11 <torre.michele07@gmail.com>
2025-08-04 19:18:58 +0000

    Fixed build error

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,6 @@ const nextConfig: NextConfig = {
     removeConsole: process.env.NODE_ENV === "production",
   },
   experimental: {
-    optimizeCss: true,
     optimizePackageImports: ["lucide-react"],
   },
 };
