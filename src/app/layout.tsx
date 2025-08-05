2c31e7e943d17486612025386ae1186de47b21f3
Michele <torre.michele07@gmail.com>
2025-08-05 10:18:55 +0000

    Edited og image size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
       {
         url: "https://michelemanna.me/michelemanna-logo.png",
         width: 1200,
-        height: 1200,
+        height: 630,
         alt: "Michele's Portfolio",
       },
     ],
